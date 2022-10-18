6899e0901db6e8e40ecf261cc720c57c12c0d619
SubediKodes <100614524+SubediKodes@users.noreply.github.com>
2022-10-19 01:01:32 +0200

    good

--- HTMLCSS/js/energie.js
+++ HTMLCSS/js/energie.js
@@ -1,15 +1,15 @@
 var meterList = [
-    {'img_src': "img/gas-icon.svg"},
-    {'img_src': "img/electricity-icon.svg"},
-    {'img_src': "img/electricity-icon.svg"}
+    {'img_src': "img/gas-icon.svg",'text': "Gas Meter"},
+    {'img_src': "img/electricity-icon.svg",'text': "Elektriciteit"},
+    {'img_src': "img/solar-icon.svg",'text': "Zonnepanelen "}
 ]
 var toestelList = [
-    {'img_src': "img/droogkast.svg"},
-    {'img_src': "img/kookplaat.svg"},
-    {'img_src': "img/oven.svg"},
-    {'img_src': "img/vaatwas.svg"},
-    {'img_src': "img/wasmachine.svg"},
-    {'img_src': "img/waterkoker.svg"}
+    {'img_src': "img/droogkast.svg",'text': "Droogkast"},
+    {'img_src': "img/kookplaat.svg",'text': "Kookplaat"},
+    {'img_src': "img/oven.svg",'text': "Oven"},
+    {'img_src': "img/vaatwas.svg",'text':'Vaatwas'},
+    {'img_src': "img/wasmachine.svg",'text':'Wasmachine'},
+    {'img_src': "img/waterkoker.svg",'text':'Waterkoker'}
 ]
 
 function vulmeterList(){
@@ -17,7 +17,8 @@ function vulmeterList(){
     let li = document.getElementById('meter')
     for (let meter of meterList){
         li = document.createElement('li')
-        li.appendChild(document.createTextNode(meter.img_src))
+        li.setAttribute('class','list-group-item')
+        li.appendChild(document.createTextNode(meter.text))
         ul.appendChild(li)
     }
     
@@ -27,11 +28,12 @@ function vulToestelLijst(){
     let li = document.getElementById('toestel')
     for (let toestel of toestelList){
         li = document.createElement('li')
+        li.setAttribute('class','list-group-item')
         var img = document.createElement('img')
         img.src = toestel.img_src
         img.style.height = '14px'
         li.appendChild(img)
-        li.appendChild(document.createTextNode(toestel.img_src))
+        li.appendChild(document.createTextNode(toestel.text))
         ul.appendChild(li)
     }
     
